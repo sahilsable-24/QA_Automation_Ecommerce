5bb33f0427a357d3a9cabe8269aaa0627e09de9c
sahilsable-24 <sahilsable2407@gmail.com>
2026-07-19 19:10:06 +0530

    added basic login test functonality

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from pages.base_page import BasePage
+
+class LoginPage(BasePage):
+
+    def __init__(self, page):
+        super().__init__(page)
+        self.username_input = page.locator("#user-name")
+        self.password_input = page.locator("#password")
+        self.login_button = page.locator("#login-button")
+        self.error_message = page.locator('[data-test="error"]')
+
+    def login(self,username,password):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+
+    def get_error_message(self):
+        return self.error_message.inner_text()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,19 @@
 from playwright.sync_api import Page
+from pages.login_page import LoginPage
 
 
-def test_open_saucedemo(page:Page):
+# def test_open_saucedemo(page:Page):
+#     page.goto("https://www.saucedemo.com")
+#     # assert page.title() == "Swag Labs"
+
+def test_login (page):
+    login_page = LoginPage(page)
     page.goto("https://www.saucedemo.com")
-    assert page.title() == "Swag Labs"
+    login_page.login("standard_user", "secret_sauce")
+    assert page.locator(".title").inner_text() == "Products"
 
+def test_invalid_login(page):
+    login_page = LoginPage(page)
+    page.goto("https://www.saucedemo.com")
+    login_page.login("wrong_user", "wrong_pass")
+    assert "do not match" in login_page.get_error_message()    
